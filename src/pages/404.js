80d1aaf1295c679885c0b935fa9a49392f191fe3
ignasandr <ignas.an@gmail.com>
2021-03-23 11:00:22 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,18 @@
 import React from "react"
+import { Center, Text, Box, useColorMode } from "@chakra-ui/react"
+import * as styles from "./404.module.css"
+import voidCats from "../images/backgrounds/voidCats.png"
 
-const NotFound = () => (
-    <p>Not Found</p>
-)
 
-export default NotFound;
+export default function NotFound() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Center w="100%" h="70vh" flexDirection="column">
+      <Text fontWeight="200" fontSize="xl" align="justify">When you pspspspspsps into the void, sometimes it pspspspspspsps's back.</Text>
+        <Box m={5}>
+            <img src={voidCats} alt="Trans" className={colorMode === 'dark' ? styles.standard : styles.inverted}/>
+        </Box>
+    </Center>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Center, useColorMode } from "@chakra-ui/react"
-import * as containerStyles from "./about.module.css"
+import * as styles from "./about.module.css"
 import trans from "../images/backgrounds/transHuman.png"
 
 
@@ -9,8 +9,7 @@ export default function About() {
 
   return (
     <Center w="100%" h="70vh">
-      {/* <Text fontWeight="200" fontSize="3xl">Welcome to the anti-climatic collapse of your reality</Text> */}
-      <img src={trans} alt="Trans" className={colorMode === 'light' ? containerStyles.standard : containerStyles.inverted}/>
+      <img src={trans} alt="Trans" className={colorMode === 'light' ? styles.standard : styles.inverted}/>
     </Center>
   )
 }
