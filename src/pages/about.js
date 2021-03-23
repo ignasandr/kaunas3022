4339631543fb6c4baa62440b2d269ededb9760b3
ignasandr <ignas.an@gmail.com>
2021-03-23 09:11:03 +0200

    about page, fixed header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,16 @@
 import React from "react"
-import { Box } from "@chakra-ui/react"
+import { Center, useColorMode } from "@chakra-ui/react"
+import * as containerStyles from "./about.module.css"
+import trans from "../images/backgrounds/transHuman.png"
+
 
 export default function About() {
+  const { colorMode } = useColorMode();
+
   return (
-    <Box>About us</Box>
+    <Center w="100%" h="70vh">
+      {/* <Text fontWeight="200" fontSize="3xl">Welcome to the anti-climatic collapse of your reality</Text> */}
+      <img src={trans} alt="Trans" className={colorMode === 'light' ? containerStyles.standard : containerStyles.inverted}/>
+    </Center>
   )
 }
-
--- src/pages/components/sections/Header.js
+++ src/pages/components/sections/Header.js
@@ -3,52 +3,54 @@ import { Flex, HStack, Box, useColorMode, Fade } from "@chakra-ui/react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import { AnimateSharedLayout, motion } from "framer-motion"
+import { useLocation } from '@reach/router'
 
 const MenuItems = ["Home", "About", "Market", "Contact"];
 
 export default function Header () {
-    const { colorMode, toggleColorMode } = useColorMode();
-    const [ selected, setSelected ] = useState(MenuItems[0]);
-    return(
-        <Flex
-            as="nav"
-            align="center"
-            justify="space-between"
-            wrap="wrap"
-            w="100%"
-        >
-            <Box onClick={toggleColorMode} mt={5} ml={5} height={100}>
-              <Link to="/">
-                <Fade in={colorMode === 'dark'} unmountOnExit>
-                  <StaticImage src="../../../images/logos/dark.jpg" height={100} alt="Kaunas 3022" style={{position: "absolute"}}></StaticImage>
-                </Fade>
-                <Fade in={colorMode === 'light'} unmountOnExit>
-                  <StaticImage src="../../../images/logos/light.jpg" height={100} alt="Kaunas 3022" style={{position: "absolute"}}></StaticImage>
-                </Fade>
-               </Link>
-            </Box>
-            <AnimateSharedLayout>
-              <HStack spacing="5" mr={10} fontSize="2xl" fontWeight="400">
-                { MenuItems.map((name) => {
-                  const trimmed = "/" + name.replace(/\s+/g, '').toLowerCase();
-                  return(<Item
-                        trimmed={trimmed}
-                        name={name}
-                        isSelected={selected === name}
-                        color={colorMode === 'dark' ? 'red' : '#3182CE'}
-                        onClick={() => {
-                          setSelected(name);
-                        }}
-                      />)
-                })}
-              </HStack>
-            </AnimateSharedLayout>
-        </Flex>);
+  const location = useLocation();
+  console.log(location.pathname);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [ selected, setSelected ] = useState(location.pathname);
+
+  return(
+      <Flex
+          as="nav"
+          align="center"
+          justify="space-between"
+          wrap="wrap"
+          w="100%"
+      >
+          <Box onClick={toggleColorMode} mt={5} ml={5} height={100} cursor={'pointer'}>
+              <Fade in={colorMode === 'dark'} unmountOnExit>
+                <StaticImage src="../../../images/logos/dark.jpg" height={100} alt="Kaunas 3022" style={{position: "absolute"}}></StaticImage>
+              </Fade>
+              <Fade in={colorMode === 'light'} unmountOnExit>
+                <StaticImage src="../../../images/logos/light.jpg" height={100} alt="Kaunas 3022" style={{position: "absolute"}}></StaticImage>
+              </Fade>
+          </Box>
+          <AnimateSharedLayout>
+            <HStack spacing="5" mr={10} fontSize="2xl" fontWeight="400">
+              { MenuItems.map((name) => {
+                const trimmed = "/" + name.replace(/\s+/g, '').toLowerCase();
+                return(<Item
+                      key={trimmed}
+                      trimmed={trimmed}
+                      name={name}
+                      isSelected={selected === trimmed}
+                      color={colorMode === 'dark' ? 'red' : '#3182CE'} //Blue.500
+                      onClick={() => {
+                        setSelected(trimmed);
+                      }}
+                    />)
+              })}
+            </HStack>
+          </AnimateSharedLayout>
+      </Flex>);
 
 }
 
 const Item = ({ trimmed, name, isSelected, color, onClick}) => {
-  console.log(trimmed, name, isSelected, color);
   return (
     <Link key={trimmed} to={name === "Home" ? "/" : trimmed} style={{ position: 'relative' }} onClick={onClick}>
       <Box _hover={{ color: `${color}`}} color={isSelected ? `${color}` : '' }>
